8f49f2d29e0fad71eca884109ca2708c8fd9fdd0
Minho Kim <bewriter310@gmail.com>
2022-10-02 12:48:08 +0900

    cors test

--- src/main/java/com/example/myfirstmac/controller/HelloController.java
+++ src/main/java/com/example/myfirstmac/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.myfirstmac.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
