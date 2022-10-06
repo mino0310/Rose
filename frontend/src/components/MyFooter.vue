ac4a96b60177f6e64ae520926d2b6501973675e0
Minho Kim <bewriter310@gmail.com>
2022-10-07 00:17:19 +0900

    footer icon href

--- frontend/src/components/MyFooter.vue
+++ frontend/src/components/MyFooter.vue
@@ -11,9 +11,11 @@
       <v-card-text>
         <v-btn
             v-for="icon in icons"
-            :key="icon"
+            :key="icon.name"
             class="mx-4 white--text"
             icon
+            :href="icon.link"
+            target="_blank"
         >
           <v-icon size="24px">
             {{ icon.name }}
@@ -21,12 +23,11 @@
         </v-btn>
       </v-card-text>
 
-      <v-card-text class="white--text pt-0">
-        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut
-        risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui.
-        Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices,
-        cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque
-        penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      <v-card-text class="white--text">
+        너는 할 수 있어. 나는 할 수 있어. 반드시 해낼 수 있어. 해낼 거야!!!!!!!!!!
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       </v-card-text>
 
       <v-divider></v-divider>
@@ -41,7 +42,16 @@
 export default {
   name: 'myFooter',
   props: {
-    icons: {}
+  },
+  data(){
+    return {
+      icons: [
+        {name:'mdi-facebook', link:'https://www.facebook.com'},
+        {name:'mdi-twitter', link:'https://twitter.com'},
+        {name:'mdi-linkedin', link:'https://www.linkedin.com'},
+        {name:'mdi-instagram', link:'https://www.instagram.com'},
+      ]
+    }
   }
 }
 </script>
